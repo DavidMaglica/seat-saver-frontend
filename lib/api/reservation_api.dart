ded8f8a4e799cfc84d8424cc9e668c66a09c727f
dmaglica <60017346+DavidMaglica@users.noreply.github.com>
2025-05-19 21:51:24 +0200

    added logger

--- lib/api/reservation_api.dart
+++ lib/api/reservation_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
 
 import 'api_routes.dart';
 import 'data/api_reservation_details.dart';
@@ -6,16 +7,22 @@ import 'data/basic_response.dart';
 import 'dio_setup.dart';
 
 final dio = setupDio(ApiRoutes.reservation);
+final logger = Logger();
 
 class ReservationApi {
   Future<List<APIReservationDetails>> getReservations(String userEmail) async {
-    Response response =
-        await dio.get('${ApiRoutes.getReservations}?email=$userEmail');
-    List<APIReservationDetails> reservations = (response.data as List)
-        .map((reservation) => APIReservationDetails.fromJson(reservation))
-        .toList();
+    try {
+      Response response =
+          await dio.get('${ApiRoutes.getReservations}?email=$userEmail');
+      List<APIReservationDetails> reservations = (response.data as List)
+          .map((reservation) => APIReservationDetails.fromJson(reservation))
+          .toList();
 
-    return reservations;
+      return reservations;
+    } catch (e) {
+      logger.e('Error fetching reservations: $e');
+      return [];
+    }
   }
 
   Future<BasicResponse> createReservation(
@@ -34,6 +41,7 @@ class ReservationApi {
 
       return BasicResponse.fromJson(response.data);
     } catch (e) {
+      logger.e('Error creating reservation: $e');
       return BasicResponse(
         success: false,
         message: 'Failed to create reservation',
@@ -50,6 +58,7 @@ class ReservationApi {
           '${ApiRoutes.deleteReservation}?email=$email&reservationId=$reservationId');
       return BasicResponse.fromJson(response.data);
     } catch (e) {
+      logger.e('Error deleting reservation: $e');
       return BasicResponse(
         success: false,
         message: 'Failed to delete reservation',
