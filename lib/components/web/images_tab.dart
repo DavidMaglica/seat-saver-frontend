1f423f5ab3fa1acb33111660f43504c11308361a
dmaglica <60017346+DavidMaglica@users.noreply.github.com>
2025-07-29 21:25:20 +0200

    mc

--- lib/components/web/images_tab.dart
+++ lib/components/web/images_tab.dart
@@ -50,29 +50,7 @@ class ImagesTab extends StatelessWidget {
     );
   }
 
-  Widget _buildButton(
-    BuildContext context,
-    String label,
-    Function() onPressed,
-  ) {
-    return FFButtonWidget(
-      onPressed: onPressed,
-      text: label,
-      options: FFButtonOptions(
-        width: 198,
-        height: 50,
-        color: WebTheme.successColor,
-        textStyle: const TextStyle(
-          fontSize: 16,
-          color: Color(0xFFFFFBF4),
-        ),
-        elevation: 3,
-        borderRadius: BorderRadius.circular(8),
-      ),
-    );
-  }
-
-  Expanded _buildPageView(BuildContext context) {
+  Widget _buildPageView(BuildContext context) {
     return Expanded(
       child: SizedBox(
         width: double.infinity,
@@ -83,12 +61,8 @@ class ImagesTab extends StatelessWidget {
               controller: _model.pageViewController,
               scrollDirection: Axis.horizontal,
               children: [
-                _buildVenueImages(
-                  context,
-                  'Venue Images',
-                  _model.venueImages,
-                ),
-                _buildMenuImages(context),
+                _buildImages(context, 'Venue Images', _model.venueImages),
+                _buildImages(context, 'Menu Images', _model.menuImages),
               ],
             ),
             _buildPageIndicator(context),
@@ -98,72 +72,7 @@ class ImagesTab extends StatelessWidget {
     );
   }
 
-  Column _buildMenuImages(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        _buildTitle(context, 'Menu Images'),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: GridView(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 12,
-              mainAxisSpacing: 12,
-              childAspectRatio: 1,
-            ),
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            children: [
-              Material(
-                color: Colors.transparent,
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Container(
-                  width: 300,
-                  constraints: const BoxConstraints(
-                    maxWidth: 800,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surface,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Expanded(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.network(
-                              'https://images.unsplash.com/photo-1470162656305-6f429ba817bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwyMnx8dGVhfGVufDB8fHx8MTcwNjU2MTE2OHww&ixlib=rb-4.0.3&q=80&w=400',
-                              width: double.infinity,
-                              height: 200,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ].divide(const SizedBox(height: 8)),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ).animateOnPageLoad(
-              _model.animationsMap['textOnPageLoadAnimation5']!),
-        ),
-      ],
-    );
-  }
-
-  Column _buildVenueImages(
-    BuildContext context,
-    String label,
-    List<String> images,
-  ) {
+  Widget _buildImages(BuildContext context, String label, List<String> images) {
     return Column(
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -181,29 +90,29 @@ class ImagesTab extends StatelessWidget {
             shrinkWrap: true,
             scrollDirection: Axis.vertical,
             children:
-            // images.map((imageUrl) {
-            //       return Material(
-            //         color: Theme.of(context).colorScheme.onSurface,
-            //         elevation: 3,
-            //         shape: RoundedRectangleBorder(
-            //           borderRadius: BorderRadius.circular(8),
-            //         ),
-            //         child: Padding(
-            //           padding: const EdgeInsets.all(8),
-            //           child: ClipRRect(
-            //             borderRadius: BorderRadius.circular(8),
-            //             child: Image.network(
-            //               'https://images.unsplash.com/photo-1470162656305-6f429ba817bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwyMnx8dGVhfGVufDB8fHx8MTcwNjU2MTE2OHww&ixlib=rb-4.0.3&q=80&w=400',
-            //               width: double.infinity,
-            //               height: 200,
-            //               fit: BoxFit.cover,
-            //             ),
-            //           ),
-            //         ),
-            //       );
-            //     })
-            //     .toList(),
-            [
+                // images.map((imageUrl) {
+                //       return Material(
+                //         color: Theme.of(context).colorScheme.onSurface,
+                //         elevation: 3,
+                //         shape: RoundedRectangleBorder(
+                //           borderRadius: BorderRadius.circular(8),
+                //         ),
+                //         child: Padding(
+                //           padding: const EdgeInsets.all(8),
+                //           child: ClipRRect(
+                //             borderRadius: BorderRadius.circular(8),
+                //             child: Image.network(
+                //               'https://images.unsplash.com/photo-1470162656305-6f429ba817bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwyMnx8dGVhfGVufDB8fHx8MTcwNjU2MTE2OHww&ixlib=rb-4.0.3&q=80&w=400',
+                //               width: double.infinity,
+                //               height: 200,
+                //               fit: BoxFit.cover,
+                //             ),
+                //           ),
+                //         ),
+                //       );
+                //     })
+                //     .toList(),
+                [
               Material(
                 color: Theme.of(context).colorScheme.onSurface,
                 elevation: 3,
@@ -264,6 +173,28 @@ class ImagesTab extends StatelessWidget {
           activeDotColor: WebTheme.accent1,
           paintStyle: PaintingStyle.stroke,
         ),
+      ),
+    );
+  }
+
+  Widget _buildButton(
+      BuildContext context,
+      String label,
+      Function() onPressed,
+      ) {
+    return FFButtonWidget(
+      onPressed: onPressed,
+      text: label,
+      options: FFButtonOptions(
+        width: 198,
+        height: 50,
+        color: WebTheme.successColor,
+        textStyle: const TextStyle(
+          fontSize: 16,
+          color: Color(0xFFFFFBF4),
+        ),
+        elevation: 3,
+        borderRadius: BorderRadius.circular(8),
       ),
     );
   }
